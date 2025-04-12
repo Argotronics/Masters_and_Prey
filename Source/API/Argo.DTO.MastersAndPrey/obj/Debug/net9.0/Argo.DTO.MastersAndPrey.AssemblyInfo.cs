50cffee201636a88ee34d2e7c6f0c812cda8dd3e
Argotronics <ArgotronicsDev@gmail.com>
2025-04-12 11:52:41 -0500

    added gitignore and proper project

--- Source/API/Argo.DTO.MastersAndPrey/obj/Debug/net9.0/Argo.DTO.MastersAndPrey.AssemblyInfo.cs
+++ Source/API/Argo.DTO.MastersAndPrey/obj/Debug/net9.0/Argo.DTO.MastersAndPrey.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Argo.DTO.MastersAndPrey")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5041bfbb864571147ee33993219555ec49bceff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c71f3b632faaf2552fc946f9ab959c8dc7622813")]
 [assembly: System.Reflection.AssemblyProductAttribute("Argo.DTO.MastersAndPrey")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Argo.DTO.MastersAndPrey")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Source/Websites/Argo.WEB.MastersAndPrey/Program.cs
+++ Source/Websites/Argo.WEB.MastersAndPrey/Program.cs
@@ -1,26 +1,28 @@
+using Argo.WEB.MastersAndPrey.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-app.UseRouting();
 
-app.UseAuthorization();
+app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
